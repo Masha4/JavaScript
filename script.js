16dc7d586c89cd0ab6ac71224e43b9c791d7bf99
Masha4 <mashatsoy.mashatsoy@gmail.com>
2018-11-25 22:42:46 +0600

    html

---
+++ script.js
@@ -0,0 +1,32 @@
+function MyFirstApp(name, age) {
+
+	alert("Привет, меня зовут " + name + " и это моя первая программа");
+	function showSkills() {
+		let skills = [' html', ' css', ' js'];
+		
+
+		for (let i = 0; i < skills.length; i++) {
+			document.write('Я владею' + skills[i] + '<br>')
+		} 
+	}
+	showSkills(); 
+	 
+	function checkAge() {
+		if (age > 18){
+			alert("Ура, тебе есть 18")
+		}
+		else {
+			alert("Тебе еще нет 18, но ничего страшного")
+		}
+	}
+	checkAge();
+
+ 
+	function calcPow(num) {
+		console.log(num * num)
+	}
+	calcPow(4)
+
+}
+
+MyFirstApp("Masha", 20);
